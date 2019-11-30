37b776ba7af724dd7d4396fb8e19c42824bc9986
hmsk <k.hamasaki@gmail.com>
2019-11-29 23:24:00 -0800

    Add another link for dynamic importing

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,13 @@
     <h1>frontmatter-markdown-loader-nuxt-sample</h1>
     <ol type="A">
       <li><nuxt-link to="/akg">Import synchronously</nuxt-link></li>
-      <li><nuxt-link to="/article?name=akg">Import dynamically</nuxt-link></li>
+      <li>
+        Import dynamically
+        <ul>
+          <li><nuxt-link to="/article?name=akg">akg.md</nuxt-link></li>
+          <li><nuxt-link to="/article?name=qurkli">quruli.md</nuxt-link></li>
+        </ul>
+      </li>
       <li><nuxt-link to="/quruli">Run component on markdown</nuxt-link></li>
     </ol>
   </div>
